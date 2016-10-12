e658d0dadb98d6a29a28c806eec463ea005901a9
Thomas Bille <thomas.bille625@gmail.com>
2016-10-12 15:13:49 +0200

    Back to nromal

--- global.js
+++ global.js
@@ -8,12 +8,4 @@ $(function() {
     });
     event.preventDefault();
   });
-
-
-
-  var map = L.map('map').setView([43.6039,1.3355], 12);
-
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
 });
