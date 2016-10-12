4e695df6c5ed9850c37d46f52d6b7bf04cbde04d
ClementChaumel <clement.chaumel@gmail.com>
2016-10-12 17:26:25 +0200

    Some changes

--- global.js
+++ global.js
@@ -1,11 +1,14 @@
 $(function() {
   mixpanel.track('connected', {});
+
   $('form').submit(function(event) {
+    event.preventDefault();
     mixpanel.identify();
     mixpanel.people.set({
       "$email": $('#email').val(),
       "$last_login": new Date()
     });
-    event.preventDefault();
+    $('.subscribe').addClass('hidden');
+    $('.thanks').removeClass('hidden');
   });
 });
